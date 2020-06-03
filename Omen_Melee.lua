395db4a2a63af0dd95f6e7ac20a0af62e8c2923b
Gonzalo Carrillo <mynameisgonz@gmail.com>
2020-06-03 10:30:16 -0400

    fixed omen_melee

--- Omen_Melee.lua
+++ Omen_Melee.lua
@@ -1,86 +1,86 @@
-	["Ou"]={
-        ["Ebullient Nullification"]={
-            ["complete_reaction"]="", 
-            ["ready_reaction"]="runaway 21"
-        },
-		["Dancing Fullers"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 11"
-        },
-		["Gardez"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 21"
-        }
-    }, 
-	["Kin"]={
-        ["Stygian Sphere"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Malign Invocation"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Diabolic Claw"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Deathly Diminuendo"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Hellish Crescendo"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Stygian Cyclone"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Interference"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Death Sentence"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        },
-		["Target"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        }
+["Ou"]={
+	["Ebullient Nullification"]={
+		["complete_reaction"]="", 
+        ["ready_reaction"]="runaway 21"
     },
-    ["Kei"]={
-        ["Dancing Fullers"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 11"
-        }
-    }, 
-    ["Fu"]={
-        ["Ebullient Nullification"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 21"
-        }
-    }, 
-	["Glassy Gorger"]={
-        ["Blessing Sync"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 11"
-        }
-    }, 
-    ["Glassy Craver"]={
-        ["View Sync"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 26"
-        }
-    }, 
-    ["Glassy Thinker"]={
-        ["Shadow Spread"]={
-            ["complete_reaction"]="runto", 
-            ["ready_reaction"]="runaway 11"
-        }, 
-        ["Pain Sync"]={
-            ["complete_reaction"]="facemob", 
-            ["ready_reaction"]="turnaround"
-        }
+	["Dancing Fullers"]={
+		["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 11"
+    },
+	["Gardez"]={
+        ["complete_reaction"]="runto", 
+		["ready_reaction"]="runaway 21"
     }
+},
+["Kin"]={
+	["Stygian Sphere"]={
+		["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Malign Invocation"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Diabolic Claw"]={
+		["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Deathly Diminuendo"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Hellish Crescendo"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+		["Stygian Cyclone"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Interference"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Death Sentence"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Target"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    }
+},
+["Kei"]={
+    ["Dancing Fullers"]={
+        ["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 11"
+    }
+},
+["Fu"]={
+    ["Ebullient Nullification"]={
+        ["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 21"
+    }
+}, 
+["Glassy Gorger"]={
+    ["Blessing Sync"]={
+        ["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 11"
+    }
+}, 
+["Glassy Craver"]={
+    ["View Sync"]={
+        ["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 26"
+    }
+}, 
+["Glassy Thinker"]={
+    ["Shadow Spread"]={
+        ["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 11"
+    }, 
+    ["Pain Sync"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    }
+}
