75635c037729f51f9c933aa31097e8aa6ca8504f
Gonzalo Carrillo <mynameisgonz@gmail.com>
2020-06-03 10:31:44 -0400

    Create Omen_Tank.lua

---
+++ Omen_Tank.lua
@@ -0,0 +1,56 @@
+["Ou"]={
+	["Dancing Fullers"]={
+		["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 11"
+    }
+},
+["Kin"]={
+	["Stygian Sphere"]={
+		["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Malign Invocation"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Diabolic Claw"]={
+		["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Deathly Diminuendo"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Hellish Crescendo"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+		["Stygian Cyclone"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Interference"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Death Sentence"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    },
+	["Target"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    }
+},
+["Kei"]={
+    ["Dancing Fullers"]={
+        ["complete_reaction"]="runto", 
+        ["ready_reaction"]="runaway 11"
+    }
+},
+["Glassy Thinker"]={
+    ["Pain Sync"]={
+        ["complete_reaction"]="facemob", 
+        ["ready_reaction"]="turnaround"
+    }
+}
